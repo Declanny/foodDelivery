2e1f082c7d34e04185be728f125e67bbd2d5b11a
Declanny <chisomhenryg@gmail.com>
2024-11-15 11:38:39 +0100

    done 2

--- src/components/restaurantPage/RestaurantPage.jsx
+++ src/components/restaurantPage/RestaurantPage.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+
 import RestaurantCard from '../shared/restaurant/RestaurantCard';
 
 const RestaurantPage = () => {
   return (
     <div className="restaurant-page">
-      
       <div className="restaurant-scroll-container">
         <RestaurantCard
           image="https://res.cloudinary.com/dqbbm0guw/image/upload/v1729973467/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_nrkzd7.jpg"
@@ -26,20 +25,21 @@ const RestaurantPage = () => {
           name="Chicken Republic"
           description="A delightful dining experience."
           link="https://restaurant1.com"
-        /><RestaurantCard
-        image="https://res.cloudinary.com/dqbbm0guw/image/upload/v1729973467/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_nrkzd7.jpg"
-        logo="https://res.cloudinary.com/dqbbm0guw/image/upload/v1731399064/ChickenRepublic_Logo_ar2bwm.svg"
-        name="Chicken Republic"
-        description="A delightful dining experience."
-        link="https://restaurant1.com"
-      /><RestaurantCard
-      image="https://res.cloudinary.com/dqbbm0guw/image/upload/v1729973467/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_nrkzd7.jpg"
-      logo="https://res.cloudinary.com/dqbbm0guw/image/upload/v1731399064/ChickenRepublic_Logo_ar2bwm.svg"
-      name="Chicken Republic"
-      description="A delightful dining experience."
-      link="https://restaurant1.com"
-    />
-        {/* Additional RestaurantCard components as needed */}
+        />
+        <RestaurantCard
+          image="https://res.cloudinary.com/dqbbm0guw/image/upload/v1729973467/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_nrkzd7.jpg"
+          logo="https://res.cloudinary.com/dqbbm0guw/image/upload/v1731399064/ChickenRepublic_Logo_ar2bwm.svg"
+          name="Chicken Republic"
+          description="A delightful dining experience."
+          link="https://restaurant1.com"
+        />
+        <RestaurantCard
+          image="https://res.cloudinary.com/dqbbm0guw/image/upload/v1729973467/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table_nrkzd7.jpg"
+          logo="https://res.cloudinary.com/dqbbm0guw/image/upload/v1731399064/ChickenRepublic_Logo_ar2bwm.svg"
+          name="Chicken Republic"
+          description="A delightful dining experience."
+          link="https://restaurant1.com"
+        />
       </div>
     </div>
   );
